cd50dc1bf6e8ec7ecdec83a8d7f02a3b99f69a6a
root <root@ip-172-31-16-237.ap-southeast-1.compute.internal>
2024-05-30 11:58:47 +0000

    firstcommit

--- checkoutcode.groovy
+++ checkoutcode.groovy
@@ -1,6 +1,6 @@
-def call(String https://github.com/ninghotnikhil00/shared-lib.git, String master){
+def call(String https://github.com/ninghotnikhil00/shared-lib.git, String main){
 def workingDir = "${env.WORKSPACE}"
 sh "git clone ${https://github.com/ninghotnikhil00/shared-lib.git} ${workingDir}"
-sh "git checkout ${master}"
+sh "git checkout ${main}"
 return workingDir
 }
